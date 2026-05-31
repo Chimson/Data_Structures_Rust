61bfbdd7270559478ce267634b34ffacf73b3894
Chimson <bensonharkison10@hotmail.com>
2026-05-30 22:32:10 -0500

    removed target folds

--- RustVim/learn_rust_exec/target/debug/deps/learn_rust_exec-aea200472a89ea06.d
+++
@@ -1,5 +0,0 @@
-/Users/bharki/Repos/Data_Structures_Rust/RustVim/learn_rust_exec/target/debug/deps/learn_rust_exec-aea200472a89ea06.d: src/main.rs
-
-/Users/bharki/Repos/Data_Structures_Rust/RustVim/learn_rust_exec/target/debug/deps/learn_rust_exec-aea200472a89ea06: src/main.rs
-
-src/main.rs:
--- RustVim/learn_rust_exec/target/debug/deps/learn_rust_exec-bfb3b76be083bda8.d
+++
@@ -1,9 +0,0 @@
-/Users/bharki/Repos/Data_Structures_Rust/RustVim/learn_rust_exec/target/debug/deps/learn_rust_exec-bfb3b76be083bda8.d: src/main.rs Cargo.toml
-
-/Users/bharki/Repos/Data_Structures_Rust/RustVim/learn_rust_exec/target/debug/deps/liblearn_rust_exec-bfb3b76be083bda8.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
